2ac4477ef57283d9200fbe4acbe60d47fad9fabf
= <=>
2025-05-14 21:00:22 -0300

    commit - 14/05 - usuario

--- unifood_app/models/usuario.py
+++ unifood_app/models/usuario.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.conf import settings
+#from django.contrib.auth.models import User
 
 class Usuario(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-
-    def __str__(self):
-        return self.nome
