764a5346e74f015c797dc10aa861e91303befd11
ViniCosta1 <vinicosta154@hotmail.com>
2025-05-21 20:19:25 -0300

    teste2

--- unifood_app/urls.py
+++ unifood_app/urls.py
@@ -4,5 +4,6 @@ from .views import usuario
 urlpatterns = [
     path('login/', usuario.Login, name="login"),
     path('registrar/', usuario.Registrar, name="registrar"),
+    path('logout/', usuario.Logout, name="logout"),
 
 ]
