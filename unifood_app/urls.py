5e25a90e0690476bde6cad0231eb6be2b09f7b49
Nycolas2906 <160323249+Nycolas2906@users.noreply.github.com>
2025-05-22 19:54:47 -0300

    add cadastro.html - produto.py - produto.css - urls.py

--- unifood_app/urls.py
+++ unifood_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', usuario.Login, name="login"),
     path('registrar/', usuario.Registrar, name="registrar"),
     path('logout/', usuario.Logout, name="logout"),
+     path('produto/cadastro', produto.Cadastro, name="cadastro"),
     path('pedido/listar', pedidos.lista_pedidos, name="listar")
 
 ]
