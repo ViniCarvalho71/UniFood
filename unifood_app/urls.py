04749fa287a2aa09e41f2d682fdc1a01184bb023
aula <rodrigoal681@gmail.com>
2025-05-21 22:36:05 -0300

    commit - 21/05 - criacao de detalhe e listagem de pedidos

--- unifood_app/urls.py
+++ unifood_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import usuario
+from .views import usuario, pedidos
 
 urlpatterns = [
     path('login/', usuario.Login, name="login"),
     path('registrar/', usuario.Registrar, name="registrar"),
     path('logout/', usuario.Logout, name="logout"),
+    path('pedido/listar', pedidos.lista_pedidos, name="listar")
 
 ]
