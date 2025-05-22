dfbb47eae0788a3fa4ffbb53395087a1b7a93e8a
= <=>
2025-05-22 13:51:53 -0300

    commit - 22/05 - formulario de cadastro de produto

--- unifood_app/forms.py
+++ unifood_app/forms.py
@@ -16,4 +16,12 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        
     )
+
+class ProdutoForm(forms.Form):
+    nome = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    descricao = forms.CharField(max_length=200, required=True, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    preco = forms.DecimalField(max_digits=10, decimal_places=2, required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    estoque = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    foto = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
