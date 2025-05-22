caaf61e8dfcbff755ef7576ab86d441c0823a03f
paulo <phavl291106@gmail.com>
2025-05-22 20:06:25 -0300

    commit - 22/05 - corigindo urls.py

--- unifood_app/urls.py
+++ unifood_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import usuario, pedidos
+from .views import usuario, pedidos, produto
 
 urlpatterns = [
     path('login/', usuario.Login, name="login"),
     path('registrar/', usuario.Registrar, name="registrar"),
     path('logout/', usuario.Logout, name="logout"),
-     path('produto/cadastro', produto.Cadastro, name="cadastro"),
+    path('produto/cadastro', produto.Cadastro, name="cadastro"),
     path('pedido/listar', pedidos.lista_pedidos, name="listar")
 
 ]
