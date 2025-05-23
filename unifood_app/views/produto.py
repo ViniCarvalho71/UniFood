730de6807b4c425db33d0fc930703640c3148422
Nycolas2906 <160323249+Nycolas2906@users.noreply.github.com>
2025-05-23 00:41:49 +0000

    modificando cadastro e produto

--- unifood_app/views/produto.py
+++ unifood_app/views/produto.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from unifood_app.forms import ProdutoForm
 
 def Cadastro(request):
-    return render(request, "unifood_app/produto/cadastro.html")
+    form = ProdutoForm()
+    return render(request, "unifood_app/produto/cadastro.html", {'form': form})
