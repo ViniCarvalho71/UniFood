b2769f4101dd61fe0bbe22ffcca11001eb63bc55
Thiago Neves <thiagodasneves2005@gmail.com>
2025-05-28 21:58:33 -0300

    tela feed proutos

--- unifood_app/views/produto.py
+++ unifood_app/views/produto.py
@@ -4,3 +4,7 @@ from unifood_app.forms import ProdutoForm
 def Cadastro(request):
     form = ProdutoForm()
     return render(request, "unifood_app/produto/cadastro.html", {'form': form})
+
+def feed_produtos(request):
+    produtos = ProdutoForm()
+    return render(request, 'unifood_app/produto/Feed.html', {'produtos': produtos})
