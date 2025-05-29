a1febc5fb4bd059483d2180c41e57aa6deb170d5
lichwyy <phavl291106@gmail.com>
2025-05-29 16:55:44 -0300

    view, test_view adicionar ao carrinho check - outras correções

--- unifood_app/urls.py
+++ unifood_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('logout/', usuario.Logout, name="logout"),
     path('produto/cadastro', produto.Cadastro, name="cadastro"),
     path('pedido/listar', pedidos.listar_carrinho, name="listar_carrinho"),
-    path('pedido/criar_pedido', pedidos.criar_pedido, name="criar_pedido"),
+    path('pedido/adicionar_ao_carrinho', pedidos.adicionar_ao_carrinho, name="adicionar_ao_carrinho"),
     path('produto/feed_produtos', produto.feed_produtos, name="feed")
 ]
