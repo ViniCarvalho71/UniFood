9f60853c3c569e7ff625432b74542c57b0b0ac48
Nycolas2906 <160323249+Nycolas2906@users.noreply.github.com>
2025-05-29 22:17:08 -0300

    forms.py cadastro.html produto.py

--- unifood_app/views/produto.py
+++ unifood_app/views/produto.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from unifood_app.forms import ProdutoForm
 
 def Cadastro(request):
-    form = ProdutoForm()
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, request.FILES)
+        if form.is_valid():
+            produto = form.save(commit=False)
+
+            # Verifica se o usuário está autenticado antes de associar
+            if request.user.is_authenticated:
+                produto.vendedor = request.user
+
+            produto.save()
+            return redirect('cadastro')  # ou qualquer outra URL válida
+    else:
+        form = ProdutoForm()
+
     return render(request, "unifood_app/produto/cadastro.html", {'form': form})
 
 def feed_produtos(request):
