4d36d25826c1c0504630f94a0ea404931a66040e
Rodrigo <rodrigoal681@gmail.com>
2025-05-29 21:54:31 -0300

    commit 29/05 - Criação de view para listar item pedido

--- Containerfile
+++ Containerfile
@@ -6,4 +6,3 @@ RUN mkdir /app
 WORKDIR /app
 COPY . .
 EXPOSE 8000
-CMD python manage.py runserver 0.0.0.0:8000
---
+++ unifood_app/views/item_pedido.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from unifood_app.models import Pedido, Item_Pedido
+
+@login_required
+def listar_itens_pedido(request, pedido_id):
+    pedido = get_object_or_404(Pedido, pk=pedido_id, cliente=request.user)
+    itens = Item_Pedido.objects.filter(pedido=pedido)
+    return render(
+        request,
+        'unifood_app/pedidos/lista_itens_pedido.html',
+        {'pedido': pedido, 'itens': itens}
+    )
