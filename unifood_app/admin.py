379854251da095b2b89506ef0c3e16f358746bfa
lichwyy <phavl291106@gmail.com>
2025-05-31 21:31:36 -0300

    add templates corretos e fix tudo que faltava - pedidos está completo

--- unifood_app/admin.py
+++ unifood_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import usuario, pedido, produtos
+from .models import usuario, pedido, produtos, item_pedido
 
 admin.site.register(usuario.Usuario)
 admin.site.register(pedido.Pedido)
 admin.site.register(produtos.Produto)
+admin.site.register(item_pedido.Item_Pedido)
