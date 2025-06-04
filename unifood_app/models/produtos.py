0969412cef3cca4c8f11eda4483fedc9c3f1dd92
Thiago Neves <thiagodasneves2005@gmail.com>
2025-06-04 19:13:33 -0300

    testes de produtos

--- unifood_app/models/produtos.py
+++ unifood_app/models/produtos.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 
-# Modelos Produto, Pedido e Item_Pedido (sem alterações)
 class Produto(models.Model):
     vendedor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='produtos')
     nome = models.CharField(max_length=100)
@@ -11,5 +11,10 @@ class Produto(models.Model):
     estoque = models.PositiveIntegerField()
     foto = models.ImageField(upload_to='produtos/fotos/', blank=True, null=True)
 
-    def _str_(self):
-        return self.nome
+    def clean(self):
+        # Verificando se o preço é negativo
+        if self.preco < 0:
+            raise ValidationError("O preço não pode ser negativo.")
+
+    def __str__(self):
+        return self.nome  # Retornando o nome do produto no método __str__
