9d283eea60fa7f017909e5aa10b0ea0a1b5aaf89
ViniCarvalho71 <carvalhovinicius71@gmail.com>
2025-06-04 20:00:14 -0300

    Arrumando o usuário do git

--- unifood_app/views/usuario.py
+++ unifood_app/views/usuario.py
@@ -48,55 +48,7 @@ def Login(requests):
         form = LoginForm()
         return render(requests, 'unifood_app/usuario/login.html', {'form':form})
 
-def Registrar(requests):
-    if requests.method == 'POST':
-        form = RegisterForm(requests.POST)
 
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            confirm_password = form.cleaned_data['confirm_password']
-            ra = form.cleaned_data['ra']
-
-            if password != confirm_password:
-                messages.error(requests,f'As senhas devem ser iguais.')
-                return render(requests, 'unifood_app/usuario/registrar.html', {
-                    'form': form,
-                    'error': 'As senhas devem ser iguais.'
-                })
-            if Usuario.objects.filter(ra=ra).exists():
-                messages.error(requests,f'RA já cadastrado.')
-                return render(requests, 'unifood_app/usuario/registrar.html', {
-                    'form': form,
-                    'error': 'RA já cadastrado.'
-                })
-            if Usuario.objects.filter(user__username=username).exists():
-                messages.error(requests,f'Usuário já cadastrado.')
-                return render(requests, 'unifood_app/usuario/registrar.html', {
-                    'form': form,
-                    'error': 'Usuário já cadastrado.'
-                })
-            
-            if Usuario.objects.filter(user__email=email).exists():
-                messages.error(requests,f'Email já cadastrado.')
-                return render(requests, 'unifood_app/usuario/registrar.html', {
-                    'form': form,
-                    'error': 'Email já cadastrado.'
-                })
-            
-            User = get_user_model()
-            user = User.objects.create_user(
-                username=username,
-                email=email,
-                password=password,
-            )
-
-            Usuario.objects.create( 
-                user=user,
-                ra=ra,
-            )
-            return redirect('/login/')
     elif requests.method == 'GET':
         form = RegisterForm()
         return render(requests, 'unifood_app/usuario/registrar.html', {'form': form})
